62dcba197202f33e5b8e98acc90eeb30f2bd9443
Sanjida Akter <sanjidakter2304@gmail.com>
2023-08-17 19:03:15 +0600

    remove & update working

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
 
 @Injectable()
 export class UsersService {
   constructor(@InjectRepository(User) private repo: Repository<User>) {}
+
   create(email: string, password: string) {
     const user = this.repo.create({ email, password });
 
@@ -17,21 +18,22 @@ export class UsersService {
   }
 
   find(email: string) {
-    return this.repo.find({ where: { email: email } });
+    return this.repo.find({ where: { email } });
   }
 
   async update(id: number, attrs: Partial<User>) {
     const user = await this.findOne(id);
     if (!user) {
-      throw new Error('user not found!');
+      throw new Error('user not found');
     }
     Object.assign(user, attrs);
     return this.repo.save(user);
   }
+
   async remove(id: number) {
     const user = await this.findOne(id);
     if (!user) {
-      throw new Error('user not found!');
+      throw new Error('user not found');
     }
     return this.repo.remove(user);
   }
