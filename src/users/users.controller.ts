f27708b07cdb97e326b1297a474df392f43b1ec7
Sanjida Akter <sanjidakter2304@gmail.com>
2023-08-17 20:20:47 +0600

    hide password using exclude & interceptor

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,6 +7,9 @@ import {
   Param,
   Query,
   Delete,
+  NotFoundException,
+  UseInterceptors,
+  ClassSerializerInterceptor,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
@@ -21,9 +24,14 @@ export class UsersController {
     this.usersService.create(body.email, body.password);
   }
 
+  @UseInterceptors(ClassSerializerInterceptor)
   @Get('/:id')
-  findUser(@Param('id') id: string) {
-    return this.usersService.findOne(parseInt(id));
+  async findUser(@Param('id') id: string) {
+    const user = this.usersService.findOne(parseInt(id));
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
+    return user;
   }
 
   @Get()
@@ -32,12 +40,12 @@ export class UsersController {
   }
 
   @Delete('/:id')
-  removeUser(@Param('id') id:string){
+  removeUser(@Param('id') id: string) {
     return this.usersService.remove(parseInt(id));
   }
 
   @Patch('/:id')
-  updateUser(@Param('id') id:string, @Body() body:UpdateUserDto){
-    return this.usersService.update(parseInt(id),body);
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.usersService.update(parseInt(id), body);
   }
 }
